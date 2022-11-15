5dfb8543da1cf0e833299f056179457c48a416c4
Jonas Oliveira <jonasoliveira.webdeveloper@gmail.com>
2022-11-15 10:18:15 -0300

    Criou a classe User

---
+++ src/main/java/com/jonas/api/domain/User.java
@@ -0,0 +1,25 @@
+package com.jonas.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
