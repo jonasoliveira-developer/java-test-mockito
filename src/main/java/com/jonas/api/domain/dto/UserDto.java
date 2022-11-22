3f9f6a39edf186cd55cf561ec3643cb50137055b
Jonas Oliveira <jonasoliveira.webdeveloper@gmail.com>
2022-11-22 16:03:46 -0300

    Classe UserDto

---
+++ src/main/java/com/jonas/api/domain/dto/UserDto.java
@@ -0,0 +1,23 @@
+package com.jonas.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDto {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
