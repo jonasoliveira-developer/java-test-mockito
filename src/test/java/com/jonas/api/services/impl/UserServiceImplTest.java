b0f14d291ec131cc757917310813ff02d29035c6
Jonas Oliveira <jonasoliveira.webdeveloper@gmail.com>
2022-12-15 17:31:29 -0300

    teste para classe user service metodo update success

--- src/test/java/com/jonas/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/jonas/api/services/impl/UserServiceImplTest.java
@@ -114,7 +114,18 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.update(userDto);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+
     }
 
     @Test
