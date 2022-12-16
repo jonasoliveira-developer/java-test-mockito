28730cac57fcd0d48f811933015220cc4eda95ce
Jonas Oliveira <jonasoliveira.webdeveloper@gmail.com>
2022-12-16 15:56:04 -0300

    removendo anotacoes nao ultilizadas

--- src/main/java/com/jonas/api/domain/dto/UserDto.java
+++ src/main/java/com/jonas/api/domain/dto/UserDto.java
@@ -1,19 +1,12 @@
 package com.jonas.api.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 public class UserDto {
--- src/main/java/com/jonas/api/resources/exceptions/StandardError.java
+++ src/main/java/com/jonas/api/resources/exceptions/StandardError.java
@@ -2,12 +2,10 @@ package com.jonas.api.resources.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class StandardError {
     private LocalDateTime timestamp;
